50263720a21e2d14c25c33d9f535426829e54886
Habby Olusesi <habbyolu@gmail.com>
2020-11-19 11:19:15 -0500

    created routes, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # NoteTaker
+
+# TEAM SUMMARY
+
+## Description
+
+This application can be used to write, save and delete notes. The user can write any note with a title and content and can then save that note. That data will then persist even if the page is refresehd. The same is true if they choose to delete any given note.
+
+## Table of Contents
+
+* [Project Summary](#description)
+* [Preview](#preview)
+* [Deployed App](#app)
+* [Contact Me](#questions)
+  
+## Preview
+
+Below are some example images of the deployed app!
+
+![NotesExample](./assets/img/NoteTakerHome.png)
+![NotesExample2](./assets/img/NoteTakerNotes.png)
+
+## App
+
+Below is a link to the depoyed app on Heroku:
+
+[Note Taker App](https://handy-note-taker.herokuapp.com/)
+
+## Questions?
+
+If you have any questions, please don't hesitate to reach out!
+
+Github: [habby-bit](https://github.com/habby-bit)
+  
+Email: [habbyolu@gmail.com](habbyolu@gmail.com)
---
+++ routes/apiRoutes.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+var noteData = fs.readFileSync("./db/db.json", "utf8");
+var notes = JSON.parse(noteData)
+
+module.exports = function (app) {
+
+    // Gets all the saved notes data
+    app.get("/api/notes", function (req, res) {
+        res.json(notes);
+    });
+    
+    app.get("/api/notes/:id", function (req, res) {
+        res.json(notes[Number(req.params.id)]);
+    });
+
+    // Adds new notes to the existing notes data
+    app.post("/api/notes", function (req, res) {
+        var newNote = req.body;
+        var noteId = (notes.length).toString();
+        newNote.id = noteId;
+        notes.push(newNotes);
+
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes), function (err) {
+            if (err) throw (err);
+        });
+
+        res.json(notes);
+    });
+
+    // Deletes notes from the existing notes data
+    app.delete("/api/notes/:id", function (req, res) {
+        var pickedNote = req.params.id;
+        var idNum = 0;
+
+        notes = notes.filter(current => {
+            return current.id != pickedNote;
+        });
+
+        for (current of notes) {
+            current.id = idNum.toString();
+            idNum++;
+        }
+
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+        res.json(notes);
+    });
+}
